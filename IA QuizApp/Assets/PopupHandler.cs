3fa17bdf08a8063400d5c367c8903129e4c6fac8
Vikram Apilla <vikramapilla@gmail.com>
2020-03-10 22:34:31 +0100

    created QuestionUI Prefabs

---
+++ IA QuizApp/Assets/PopupHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Doozy.Engine.UI;
+
+public class PopupHandler : MonoBehaviour
+{
+    [SerializeField]
+    UIPopup popup;
+
+    public void ShowPopup()
+    {
+        popup.Show();
+    }
+
+    public void HidePopup()
+    {
+        popup.Hide();
+    }
+}
