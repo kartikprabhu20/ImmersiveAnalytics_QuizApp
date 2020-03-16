277543597338e7831f0c999529ca02dc2ad586f5
Vikram Apilla <vikramapilla@gmail.com>
2020-03-16 12:02:19 +0100

    Changed CrosshairRaycaster

---
+++ IA QuizApp/Assets/Scripts/Non-AR/CrosshairController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CrosshairController : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(50, 1000)]
+    float speed;
+
+    [SerializeField]
+    FixedJoystick fixedJoystick;
+
+
+    Vector3 position;
+    RectTransform rectTransform;
+
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        position = new Vector3(fixedJoystick.Horizontal, fixedJoystick.Vertical, 0) * speed * Time.deltaTime;
+        rectTransform.Translate(position);
+    }
+}
---
+++ IA QuizApp/Assets/Scripts/Non-AR/CrosshairRaycaster.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class CrosshairRaycaster : MonoBehaviour
+{
+    [SerializeField]
+    Color highlightColor;
+    Color initialColor;
+
+    RectTransform rectTransform;
+
+    Ray ray;
+    RaycastHit hitInfo;
+    Vector3 rayOrigin;
+
+    Collider hitObject;
+    Material hitObjectMaterial;
+
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        rayOrigin = rectTransform.position;
+        ray = Camera.main.ScreenPointToRay(rayOrigin);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+        {
+            if (hitObject == null)
+            {
+                hitObject = hitInfo.collider;
+                hitObjectMaterial = hitObject.GetComponent<Renderer>().material;
+                initialColor = hitObjectMaterial.color;
+            }
+
+            hitObjectMaterial.color = highlightColor;
+        }
+        else
+        {
+            if (hitObject != null)
+            {
+                hitObjectMaterial.color = initialColor;
+                hitObject = null;
+                hitObjectMaterial = null;
+            }
+        }
+    }
+}
