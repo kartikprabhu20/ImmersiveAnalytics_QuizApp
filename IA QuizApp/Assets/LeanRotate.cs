0a37629da11ae10ea5b79345015289a6e9610e5a
Vikram Apilla <vikramapilla@gmail.com>
2020-03-24 20:07:07 +0100

    Added LeanRotate

---
+++ IA QuizApp/Assets/LeanRotate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeanRotate : MonoBehaviour
+{
+    Touch touch;
+    Vector2 touchPosition;
+    Quaternion rotationY;
+
+    float rotateSpeed = 0.1f;
+    
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if(hitInfo.collider.name == "Scatterplot")
+                {
+                    if (touch.phase == TouchPhase.Moved)
+                    {
+                        rotationY = Quaternion.Euler(0f, -touch.deltaPosition.x * rotateSpeed, 0f);
+                        transform.rotation = rotationY * transform.rotation;
+                    }
+                }
+            }
+        }
+    }
+}
