12ad13bf42fc89bd3623f46fc8c10aa310e09469
kartikprabhu20 <kartikprabhu20@yahoo.co.in>
2020-03-29 11:26:06 +0200

    #new datasets added

--- IA QuizApp/Assets/Scripts/Controller.cs
+++ IA QuizApp/Assets/Scripts/Controller.cs
@@ -12,7 +12,8 @@ public class Controller : MonoBehaviour
     public GameObject masterCanvas;
     public GameObject scatterplot;
     public GameObject scatterPlotManager;
-
+    public GameObject selector;
+    public string controlerType;
     private ITaskListener taskListener;
 
     // Start is called before the first frame update
@@ -22,7 +23,7 @@ public class Controller : MonoBehaviour
         masterCanvas.SetActive(true); //In case of AR canvas needs to be set to active
         scatterPlotManager.SetActive(true);
         taskFactory = TaskFactory.Instance; //singleton
-        taskListener = new TaskListener(taskFactory, masterCanvas,scatterPlotManager);
+        taskListener = new TaskListener(taskFactory, masterCanvas,scatterPlotManager,selector,controlerType);
         taskFactory.getTask(COUNTER).init(taskListener, masterCanvas, scatterPlotManager);
     }
 
@@ -43,14 +44,19 @@ public class Controller : MonoBehaviour
         scatterplot = plotPrefab;
     }
 
-    internal void assignMasterCanvas(GameObject masterCanvas)
+    internal void assignMasterCanvas(GameObject canvas)
     {
-        this.masterCanvas = masterCanvas;
+        this.masterCanvas = canvas;
     }
 
-    internal void assignScatterPlotManager(GameObject scatterPlotManager)
+    public void assignSelector(GameObject selectr)
     {
-        this.scatterPlotManager = scatterPlotManager;
+        this.selector = selectr;
+    }
+
+    internal void assignScatterPlotManager(GameObject scatterManager)
+    {
+        this.scatterPlotManager = scatterManager;
     }
 
     class TaskListener : ITaskListener
@@ -58,20 +64,40 @@ public class Controller : MonoBehaviour
         private TaskFactory taskFactory;
         private GameObject masterCanvas;
         private GameObject scatterPlotManager;
+        private GameObject selector;
+        private string controllerType;
 
-        public TaskListener(TaskFactory taskFactory, GameObject masterCanvas, GameObject scatterPlotManager)
+        public TaskListener(TaskFactory taskFactory, GameObject masterCanvas, GameObject scatterPlotManager, GameObject selectr, string control)
         {
             this.taskFactory = taskFactory;
             this.masterCanvas = masterCanvas;
             this.scatterPlotManager = scatterPlotManager;
+            this.selector = selectr;
+            this.controllerType = control;
         }
 
-        public void submitted()
+        void ITaskListener.submitted()
         {
-            Debug.Log("Counter:" + COUNTER);
             COUNTER++;
-            taskFactory.getTask(COUNTER).init(this, masterCanvas, scatterPlotManager);
+            if (COUNTER > 3)
+            {
+                masterCanvas.transform.Find("Overlay - Finish").gameObject.SetActive(true);
+            }
+            else
+            {
+                if(controllerType == "AR")
+                {
+                   this.selector.GetComponent<RayCastSelector>().restore();
+                }
+                Debug.Log("Counter:" + COUNTER);
+                taskFactory.getTask(COUNTER).init(this, masterCanvas, scatterPlotManager);
+            }
         }
+    }
+
+    internal void assignControllerType(string v)
+    {
+        this.controlerType = v;
     }
 }
 
